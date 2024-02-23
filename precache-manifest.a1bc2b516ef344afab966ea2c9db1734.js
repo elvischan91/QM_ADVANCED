ed7b269d2c814ff3aad0afb00104f24fbc99eae9
Elvis Chan <chung1991525@gmail.com>
2024-02-23 22:43:50 +0800

    updated URL

---
+++ precache-manifest.a1bc2b516ef344afab966ea2c9db1734.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "995ce1c88df2b5049100ea394b6f4aef",
+    "url": "https://www.elvischan91.com/QM_ADVANCED/index.html"
+  },
+  {
+    "revision": "3a2a74ed78ce9272da8b",
+    "url": "https://www.elvischan91.com/QM_ADVANCED/static/css/2.75bc9036.chunk.css"
+  },
+  {
+    "revision": "29a4327685daaadc2d6a",
+    "url": "https://www.elvischan91.com/QM_ADVANCED/static/css/main.f95f82db.chunk.css"
+  },
+  {
+    "revision": "3a2a74ed78ce9272da8b",
+    "url": "https://www.elvischan91.com/QM_ADVANCED/static/js/2.5c405132.chunk.js"
+  },
+  {
+    "revision": "800fd26c8e23bb904a376089f475ad8a",
+    "url": "https://www.elvischan91.com/QM_ADVANCED/static/js/2.5c405132.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "266838e49c016b0d746d",
+    "url": "https://www.elvischan91.com/QM_ADVANCED/static/js/3.c3eefb9d.chunk.js"
+  },
+  {
+    "revision": "29a4327685daaadc2d6a",
+    "url": "https://www.elvischan91.com/QM_ADVANCED/static/js/main.26d89d11.chunk.js"
+  },
+  {
+    "revision": "31e7b9b63a591ff4412f",
+    "url": "https://www.elvischan91.com/QM_ADVANCED/static/js/runtime-main.19ada8b6.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "https://www.elvischan91.com/QM_ADVANCED/static/media/logo.06e73328.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/qm-app-build/precache-manifest.ee7121fd6b4de0f9237419e5a093eb69.js"
+  "https://www.elvischan91.com/QM_ADVANCED/precache-manifest.a1bc2b516ef344afab966ea2c9db1734.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/qm-app-build/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://www.elvischan91.com/QM_ADVANCED/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
